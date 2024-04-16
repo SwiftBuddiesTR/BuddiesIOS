71bd8505453635d73eed7dffd4dc0080a3fecf78
Doğukaan Kılıçarslan <dogukaan.k@hotmail.com>
2024-04-16 23:33:06 +0300

    Update app tab view

--- SwiftBuddiesIOS/Sources/ContentView.swift
+++ SwiftBuddiesIOS/Sources/ContentView.swift
@@ -1,32 +1,51 @@
 import SwiftUI
 import Auth
 import Map
+import Feed
+import About
+import Contributors
 
+enum AppTab: Int, Identifiable {
+    case feed = 0
+    case map
+    case about
+    case contributors
+//    case login
+    
+    var id: Int { rawValue }
+}
 
 public struct ContentView: View {
+    @State var selectedTab: AppTab = .feed
+
     public init() {}
 
     public var body: some View {
-        TabView {
-            VStack {
-                Button(action: {
-                    Task {
-                        try? await Authenticator.shared.signIn()
-                    }
-                }, label: {
-                    Text("Login")
-                })
-            }
-            .tabItem {
-                Image(systemName: "list.bullet")
-                Text("Feed")
-            }
-            
+        TabView(selection: $selectedTab) {
+            FeedView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Feed")
+                }
+                .tag(AppTab.feed)
             MapView()
                 .tabItem {
                     Image(systemName: "map")
                     Text("Map")
                 }
+                .tag(AppTab.map)
+            AboutView()
+                .tabItem {
+                    Image(systemName: "info.circle")
+                    Text("About")
+                }
+                .tag(AppTab.about)
+            ContributorsView()
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Contributors")
+                }
+                .tag(AppTab.contributors)
         }
     }
 }
