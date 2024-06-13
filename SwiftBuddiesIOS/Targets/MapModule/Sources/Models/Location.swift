8734febed2f6cacbf3ed4e8b5afeb6da0fcd7361
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-06-14 01:06:44 +0300

    EventModel created for SwiftData

--- SwiftBuddiesIOS/Targets/MapModule/Sources/Models/Location.swift
+++ SwiftBuddiesIOS/Targets/MapModule/Sources/Models/Location.swift
@@ -6,21 +6,26 @@
 //
 
 import Foundation
-import SwiftUI
 import MapKit
+import SwiftData
 
-
-// Define a simple model for event
-struct Location: Identifiable {
-    let id = UUID()
+@Model
+class EventModel: Identifiable {
+    let id: String
     let category: String
     let name: String
     let aboutEvent: String
     let startDate: TimeInterval
     let dueDate: TimeInterval
     let coordinate: CLLocationCoordinate2D
-    //let image: Image? = nil
-    //let backgroundColor: Color? = nil
     
-    
+    init(category: String, name: String, aboutEvent: String, startDate: TimeInterval, dueDate: TimeInterval, coordinate: CLLocationCoordinate2D) {
+        self.id = UUID().uuidString
+        self.category = category
+        self.name = name
+        self.aboutEvent = aboutEvent
+        self.startDate = startDate
+        self.dueDate = dueDate
+        self.coordinate = coordinate
+    }
 }
