58aa2540e2364d94b4ef1f0a1ef8d33e00c15e5c
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-07-22 19:05:44 +0300

    EventListView Created and UI Updated

---
+++ SwiftBuddiesIOS/Targets/MapModule/Sources/MapView/ViewComponents/EventListView.swift
@@ -0,0 +1,18 @@
+//
+//  EventListView.swift
+//  Map
+//
+//  Created by Oğuzhan Abuhanoğlu on 22.07.2024.
+//
+
+import SwiftUI
+
+struct EventListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventListView()
+}
