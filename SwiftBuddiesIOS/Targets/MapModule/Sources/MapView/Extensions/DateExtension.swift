f5a30a01dd5ef968caa86f45fc62ea293f0ff3f2
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-09-12 15:07:41 +0300

    fix

--- SwiftBuddiesIOS/Targets/MapModule/Sources/MapView/Extensions/DateExtension.swift
+++ SwiftBuddiesIOS/Targets/MapModule/Sources/MapView/Extensions/DateExtension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Date {
+    
+    func toISOString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.calendar = Calendar(identifier: .gregorian)
+        return formatter.string(from: self)
+    }
+    
+}
