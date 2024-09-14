c321ded21d85c7986e6ef829c8d92e03485b268a
Doğukaan Kılıçarslan <dogukaan.k@hotmail.com>
2024-09-14 18:13:24 +0300

    Update projet file

--- Project.swift
+++ Project.swift
@@ -20,129 +20,6 @@ extension Target {
     }
 }
 
-let localizationModule = Target.featureTarget(
-    name: "Localization",
-    productName: "Localization",
-    dependencies: [],
-    hasResources: true
-)
-
-let designModule = Target.featureTarget(
-    name: "Design",
-    productName: "Design",
-    dependencies: [.target(localizationModule)],
-    hasResources: true
-)
-
-let authModule = Target.featureTarget(
-    name: "Auth",
-    productName: "Auth",
-    dependencies: [
-        .package(product: "GoogleSignIn", type: .runtime, condition: .none)
-    ]
-)
-
-let networkModule = Target.featureTarget(
-    name: "Network",
-    productName: "Network",
-    dependencies: [
-        .target(authModule),
-        .package(product: "GoogleSignIn", type: .runtime, condition: .none)
-    ]
-)
-
-let localicationCodegen = Target.target(
-    name: "LocalizationCodegen",
-    destinations: .macOS,
-    product: .commandLineTool,
-    productName: "LocalizationCodegen",
-    bundleId: "com.swiftbuddies.localization",
-    sources: ["SwiftBuddiesIOS/Targets/ScriptsModule/LocalizationCodegen/**"],
-    scripts: [],
-    dependencies: [.package(product: "ArgumentParser", type: .runtime, condition: .none)],
-    coreDataModels: [],
-    environmentVariables: [:],
-    launchArguments: [],
-    additionalFiles: [],
-    buildRules: [],
-    mergedBinaryType: .automatic,
-    mergeable: false
-)
-
-
-
-//let scriptsModule = Target.target(
-//    name: "Scripts",
-//    destinations: .macOS,
-//    product: .staticFramework,
-//    productName: "Scripts",
-//    bundleId: "com.swiftbuddies.scripts",
-//    deploymentTargets: nil,
-//    infoPlist: nil,
-//    
-//    sources: ["SwiftBuddiesIOS/Targets/ScriptsModule/**"],
-//    resources: nil,
-//    copyFiles: nil,
-//    headers: nil,
-//    entitlements: nil,
-//    scripts: [],
-//    dependencies: [.target(localicationCodegen)],
-//    settings: nil,
-//    coreDataModels: [],
-//    environmentVariables: [:],
-//    launchArguments: [],
-//    additionalFiles: [],
-//    buildRules: [],
-//    mergedBinaryType: .automatic,
-//    mergeable: false
-//)
-
-let onboardingModule = Target.featureTarget(
-    name: "Onboarding",
-    productName: "Onboarding",
-    dependencies: [.target(designModule)]
-)
-
-let loginModule = Target.featureTarget(
-    name: "Login",
-    productName: "Login",
-    dependencies: [
-        .target(designModule),
-        .target(authModule),
-        .target(networkModule),
-        .package(product: "GoogleSignIn", type: .runtime, condition: .none)
-    ]
-)
-
-let feedModule = Target.featureTarget(
-    name: "Feed",
-    productName: "Feed",
-    dependencies: [.target(designModule)]
-)
-
-let mapModule = Target.featureTarget(
-    name: "Map",
-    productName: "Map",
-    dependencies: [.target(designModule)]
-)
-
-let profileModule = Target.featureTarget(
-    name: "Profile",
-    productName: "Profile",
-    dependencies: [
-        .target(designModule),
-        .target(authModule),
-        .package(product: "GoogleSignIn", type: .runtime, condition: .none)
-    ]
-)
-
-let contributorsModule = Target.featureTarget(
-    name: "Contributors",
-    productName: "Contributors",
-    dependencies: [.target(designModule)]
-)
-
-
 let project = Project(
     name: "Buddies",
     packages: [
@@ -154,7 +31,7 @@ let project = Project(
             name: "SwiftBuddiesIOS",
             destinations: .iOS,
             product: .app,
-            bundleId: "io.tuist.SwiftBuddiesIOS",
+            bundleId: "com.swiftbuddies.SwiftBuddiesIOS",
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleShortVersionString": "1.0",
@@ -172,31 +49,165 @@ let project = Project(
             resources: ["SwiftBuddiesIOS/Resources/**"],
             dependencies: [
                 .package(product: "GoogleSignIn", type: .runtime, condition: .none),
-                .target(designModule),
-                .target(authModule),
-                .target(onboardingModule),
-                .target(loginModule),
-                .target(feedModule),
-                .target(mapModule),
-                .target(profileModule),
-                .target(contributorsModule),
-                .target(networkModule),
-//                .target(scriptsModule),
-//                .target(localicationCodegen)
-                .target(localizationModule)
+                .target(Modules.design.target),
+                .target(Modules.auth.target),
+                .target(Modules.onboarding.target),
+                .target(Modules.login.target),
+                .target(Modules.feed.target),
+                .target(Modules.map.target),
+                .target(Modules.profile.target),
+                .target(Modules.contributors.target),
+                .target(Modules.network.target),
+                .target(Modules.localization.target)
             ]
         ),
-        designModule,
-        authModule,
-        onboardingModule,
-        loginModule,
-        feedModule,
-        mapModule,
-        profileModule,
-        contributorsModule,
-        networkModule,
-//        scriptsModule,
-        localizationModule,
-        localicationCodegen
+        Modules.design.target,
+        Modules.auth.target,
+        Modules.onboarding.target,
+        Modules.login.target,
+        Modules.feed.target,
+        Modules.map.target,
+        Modules.profile.target,
+        Modules.contributors.target,
+        Modules.network.target,
+        Modules.localization.target,
+        Modules.localicationCodegen
     ]
 )
+
+enum Modules: CaseIterable {
+    case localization
+    case design
+    case network
+    case auth
+    case onboarding
+    case login
+    case feed
+    case map
+    case profile
+    case contributors
+    
+    var target: Target {
+        switch self {
+        case .localization:
+            Target.featureTarget(
+                name: "Localization",
+                productName: "Localization",
+                dependencies: [],
+                hasResources: true
+            )
+        case .design:
+            Target.featureTarget(
+                name: "Design",
+                productName: "Design",
+                dependencies: [.target(Modules.localization.target)],
+                hasResources: true
+            )
+        case .network:
+            Target.featureTarget(
+                name: "Network",
+                productName: "Network",
+                dependencies: []
+            )
+        case .auth:
+            Target.featureTarget(
+                name: "Auth",
+                productName: "Auth",
+                dependencies: [
+                    .target(Modules.network.target),
+                    .package(product: "GoogleSignIn", type: .runtime, condition: .none)
+                ]
+            )
+        case .onboarding:
+            Target.featureTarget(
+                name: "Onboarding",
+                productName: "Onboarding",
+                dependencies: [.target(Modules.design.target)]
+            )
+        case .login:
+            Target.featureTarget(
+                name: "Login",
+                productName: "Login",
+                dependencies: [
+                    .target(Modules.design.target),
+                    .target(Modules.auth.target),
+                    .target(Modules.network.target),
+                    .package(product: "GoogleSignIn", type: .runtime, condition: .none)
+                ]
+            )
+        case .feed:
+            Target.featureTarget(
+                name: "Feed",
+                productName: "Feed",
+                dependencies: [.target(Modules.design.target)]
+            )
+        case .map:
+            Target.featureTarget(
+                name: "Map",
+                productName: "Map",
+                dependencies: [.target(Modules.design.target)]
+            )
+        case .profile:
+            Target.featureTarget(
+                name: "Profile",
+                productName: "Profile",
+                dependencies: [
+                    .target(Modules.design.target),
+                    .target(Modules.auth.target),
+                    .target(Modules.network.target),
+                    .package(product: "GoogleSignIn", type: .runtime, condition: .none)
+                ]
+            )
+        case .contributors:
+            Target.featureTarget(
+                name: "Contributors",
+                productName: "Contributors",
+                dependencies: [.target(Modules.design.target)]
+            )
+        }
+    }
+    
+    static let localicationCodegen = Target.target(
+        name: "LocalizationCodegen",
+        destinations: .macOS,
+        product: .commandLineTool,
+        productName: "LocalizationCodegen",
+        bundleId: "com.swiftbuddies.localization",
+        sources: ["SwiftBuddiesIOS/Targets/ScriptsModule/LocalizationCodegen/**"],
+        scripts: [],
+        dependencies: [.package(product: "ArgumentParser", type: .runtime, condition: .none)],
+        coreDataModels: [],
+        environmentVariables: [:],
+        launchArguments: [],
+        additionalFiles: [],
+        buildRules: [],
+        mergedBinaryType: .automatic,
+        mergeable: false
+    )
+}
+
+//let scriptsModule = Target.target(
+//    name: "Scripts",
+//    destinations: .macOS,
+//    product: .staticFramework,
+//    productName: "Scripts",
+//    bundleId: "com.swiftbuddies.scripts",
+//    deploymentTargets: nil,
+//    infoPlist: nil,
+//
+//    sources: ["SwiftBuddiesIOS/Targets/ScriptsModule/**"],
+//    resources: nil,
+//    copyFiles: nil,
+//    headers: nil,
+//    entitlements: nil,
+//    scripts: [],
+//    dependencies: [.target(localicationCodegen)],
+//    settings: nil,
+//    coreDataModels: [],
+//    environmentVariables: [:],
+//    launchArguments: [],
+//    additionalFiles: [],
+//    buildRules: [],
+//    mergedBinaryType: .automatic,
+//    mergeable: false
+//)
